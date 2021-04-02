67a68efa8dd24e4bdf0166f942a819f0d51f4543
Katherine Jones <jones.kjg@gmail.com>
2021-04-01 21:27:36 -0400

    add redux functionality

--- App.js
+++ App.js
@@ -2,13 +2,18 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import AddEntry from './components/AddEntry';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux';
+import reducer from './reducers'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <AddEntry alreadyLogged={true}/>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={createStore(reducer)}>
+      <View style={styles.container}>
+        <AddEntry alreadyLogged={true}/>
+        <StatusBar style="auto" />
+      </View>
+    </Provider>
   );
 }
 
