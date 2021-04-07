2929a4690c84d2cc4ace610803318a7e569b4dba
Katherine Jones <jones.kjg@gmail.com>
2021-04-06 22:01:34 -0400

    minor changes and debugging

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import AddEntry from './components/AddEntry';
+import History from './components/History'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux';
 import reducer from './reducers'
@@ -9,9 +10,9 @@ import reducer from './reducers'
 export default function App() {
   return (
     <Provider store={createStore(reducer)}>
-      <View style={styles.container}>
-        <AddEntry alreadyLogged={true}/>
-        <StatusBar style="auto" />
+      <View style={{flex: 1}}>
+        <History />
+        {/*<StatusBar style="auto" />*/}
       </View>
     </Provider>
   );
