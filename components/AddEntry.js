b968a607fabb469d60e4e1dc23b1dca4531fcef0
Katherine Jones <jones.kjg@gmail.com>
2021-04-06 21:57:52 -0400

    add styling for AddEntry and sub-components

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,23 +1,33 @@
 import React, { Component } from 'react'
-import { View, TouchableOpacity, Text } from 'react-native'
-import { getMetricMetaInfo, timeToString } from '../utils/helpers'
+import { 
+    View, 
+    TouchableOpacity, 
+    Text,
+    Platform,
+    StyleSheet
+ } from 'react-native'
+import { connect } from 'react-redux'
+import { getDailyReminderValue, getMetricMetaInfo, timeToString } from '../utils/helpers'
 import { submitEntry, removeEntry } from '../utils/api'
 import UdaciSlider from './UdaciSlider'
 import UdaciSteppers from './UdaciSteppers'
 import DateHeader from './DateHeader'
-import { IonIcons } from '@expo/vector-icons'
+import { Ionicons } from '@expo/vector-icons'
 import TextButton from './TextButton'
+import { addEntry } from '../actions'
+import { white, purple } from '../utils/colors'
 
 function SubmitBtn ({ onPress }) {
     return (
         <TouchableOpacity
+            style={Platform.OS === 'ios' ? styles.iosSubmitBtn : styles.AndroidSubmitBtn}
             onPress={onPress}
         >
-            <Text>Submit</Text>
+            <Text style={styles.submitBtnText}>Submit</Text>
         </TouchableOpacity>
     )
 }
-export default class AddEntry extends Component {
+class AddEntry extends Component {
     state = {
         run: 0,
         bike: 0,
@@ -29,6 +39,10 @@ export default class AddEntry extends Component {
     submit = () => {
         const key = timeToString()
         const entry = this.state
+
+        this.props.dispatch(addEntry({
+            [key]: entry
+        }))
 
         this.setState(() => ({
             run: 0,
@@ -76,33 +90,39 @@ export default class AddEntry extends Component {
     reset = () => {
         const key = timeToString()
 
+        this.props.dispatch(addEntry({
+            [key]: getDailyReminderValue()
+        }))
+
         removeEntry(key)
     }
 
     render(){
         const metaInfo = getMetricMetaInfo()
 
-        {/*
+        
         if (this.props.alreadyLogged) {
             return(
-                <View>
-                    <IonIcons name={'ios-md'} size={100} />
+                <View style={styles.center}>
+                    <Ionicons 
+                        name={Platform.OS === 'ios' ? 'ios-happy' : 'md-happy'} 
+                        size={100} />
                     <Text>You already logged your information for today</Text>
-                    <TextButton onPress={this.reset}>Reset</TextButton>
+                    <TextButton style={{padding: 10}} onPress={this.reset}>Reset</TextButton>
 
                 </View>    
             )
-        }*/}
+        }
 
         return (
-            <View>
+            <View style={styles.container}>
                 <DateHeader date={(new Date()).toLocaleDateString()}/>
             {Object.keys(metaInfo).map((key) => {
                 const { getIcon, type, ...rest } = metaInfo[key]
                 const value = this.state[key]
 
                 return (
-                    <View key={key}>
+                    <View key={key} style={styles.row}>
                         {getIcon()}
                         {type==='slider' 
                         ? <UdaciSlider 
@@ -124,4 +144,57 @@ export default class AddEntry extends Component {
             </View>
         )
     }
+   
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 20,
+        color: white,
+    },
+    row: {
+        flexDirection: 'row',
+        flex: 1,
+        alignItems: 'center'
+    },
+    iosSubmitBtn: {
+        backgroundColor: purple,
+        padding: 10, 
+        borderRadius: 7,
+        height: 45,
+        marginRight: 40,
+        marginLeft: 40
+    },
+    AndroidSubmitBtn: {
+        backgroundColor: purple,
+        paddingRight: 30,
+        height: 45,
+        borderRadius: 2,
+        alignSelf: 'flex-end',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    submitBtnText: {
+        color: white,
+        fontSize: 22,
+        textAlign: 'center'
+    },
+    center: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 30,
+        marginRight: 30
+    },
+})
+
+function mapStateToProps (state) {
+    const key = timeToString()
+
+    return{
+        alreadyLogged: state[key] && typeof state[key].today === 'undefined'
+    }
+}
+
+export default connect(mapStateToProps)(AddEntry)
--- components/UdaciSteppers.js
+++ components/UdaciSteppers.js
@@ -1,23 +1,71 @@
 import  React from 'react'
-import { View, TouchableOpacity, Text } from 'react-native'
+import { Platform, View, TouchableOpacity, Text, StyleSheet } from 'react-native'
 import { FontAwesome, Entypo } from '@expo/vector-icons'
+import { purple, white, gray } from '../utils/colors'
 
 export default function UdaciSteppers ({max, unit, step, value, onIncrement, onDecrement}) {
     return (
-        <View>
-            <View>
-                <TouchableOpacity onPress={onDecrement}>
-                    <FontAwesome name='minus' size={30} color={'black'} />
+        <View style={[styles.row, {justifyContent: 'space-between'}]}>
+            {Platform.OS==='ios'
+            ? <View style={{flexDirection: 'row'}}>
+                <TouchableOpacity 
+                    style={[styles.iosBtn, {borderBottomRightRadius: 0, borderTopRightRadius: 0}]}
+                    onPress={onDecrement}>
+                    <Entypo name='minus' size={30} color={purple} />
                 </TouchableOpacity>
 
-                <TouchableOpacity onPress={onIncrement}>
-                    <FontAwesome name='plus' size={30} color={'black'} />
+                <TouchableOpacity
+                    style={[styles.iosBtn, {borderBottomLeftRadius: 0, borderTopLeftRadius: 0, borderLeftWidth: 0}]} 
+                    onPress={onIncrement}>
+                    <Entypo name='plus' size={30} color={purple} />
                 </TouchableOpacity>
             </View>
-            <View>
-                <Text>{value}</Text>
-                <Text>{unit}</Text>
+            : <View style={{flexDirection: 'row'}}>
+                <TouchableOpacity 
+                    style={styles.androidBtn}
+                    onPress={onDecrement}>
+                    <FontAwesome name='minus' size={30} color={white} />
+                </TouchableOpacity>
+
+                <TouchableOpacity
+                    style={styles.androidBtn} 
+                    onPress={onIncrement}>
+                    <FontAwesome name='plus' size={30} color={white} />
+                </TouchableOpacity>
+            </View>}
+            <View style={styles.metricCounter}>
+                <Text style={{fontSize: 24, textAlign: 'center'}}>{value}</Text>
+                <Text style={{fontSize: 18, color: gray}}>{unit}</Text>
             </View>
+        
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    row: {
+        flexDirection: 'row',
+        flex: 1,
+        alignItems: 'center',
+    },
+    iosBtn: {
+        backgroundColor: white,
+        borderColor: purple,
+        borderWidth: 1,
+        borderRadius: 3,
+        padding: 5,
+        paddingLeft: 25,
+        paddingRight: 25,   
+    },
+    androidBtn: {
+        margin: 5,
+        padding: 10,
+        borderRadius: 2,
+        backgroundColor: purple
+    },
+    metricCounter: {
+        width: 85,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+})
