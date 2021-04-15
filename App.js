20e263cac5fc76b62eb8bff78b160be049a41aaf
kjones <jones.kjg@gmail.com>
2021-04-15 10:40:57 -0400

    install navigator and status bar dependencies; build out those components

--- App.js
+++ App.js
@@ -1,19 +1,66 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Platform } from 'react-native';
 import AddEntry from './components/AddEntry';
 import History from './components/History'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux';
 import reducer from './reducers'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { NavigationContainer } from "@react-navigation/native";
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import { FontAwesome, Ionicons } from '@expo/vector-icons'
+import { purple, white } from './utils/colors'
+import Constants from 'expo-constants'
+
+function UdaciStatusBar({backgroundColor, ...props}){
+  return (
+    <View style={{ backgroundColor, height: Constants.statusBarHeight }}>
+      <StatusBar translucent backgroundColor={backgroundColor} {...props}/>
+    </View>
+  )
+}
+
+const Tab = 
+  Platform.OS === 'ios'
+  ? createBottomTabNavigator()
+  : createMaterialTopTabNavigator()
 
 export default function App() {
   return (
     <Provider store={createStore(reducer)}>
-      <View style={{flex: 1}}>
-        <History />
-        {/*<StatusBar style="auto" />*/}
-      </View>
+      <UdaciStatusBar backgroundColor={purple} style='light'/>
+      <NavigationContainer>
+        <Tab.Navigator
+          screenOptions={({ route }) => ({
+            tabBarIcon: ({ color, size }) => {
+              let icon
+              if (route.name === 'Add Entry') {
+                icon= (
+                  <FontAwesome name='plus-square' size={size} color = {color} />
+                )
+              } else if (route.name === 'History') {
+                icon = (
+                  <Ionicons name = 'ios-bookmarks' size={size} color={color} />
+                )
+              }
+              return icon
+            },
+          })}
+          tabBarOptions={{
+            activeTintColor: Platform.OS === 'ios' ? purple : white,
+            style: {
+              backgroundColor: Platform.OS === 'ios' ? white : purple,
+            },
+            indicatorStyle: {
+              backgroundColor: 'yellow',
+            }
+          }}
+        >
+          <Tab.Screen name='History' component={History} />
+          <Tab.Screen name='Add Entry' component={AddEntry} />
+        </Tab.Navigator>
+      </NavigationContainer>
     </Provider>
   );
 }
